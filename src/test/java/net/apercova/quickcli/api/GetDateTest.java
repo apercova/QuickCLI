a458a4e9a17f4530e5d039ec06b60797b23ffebd
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:09:56 -0600

    Create GetDateTest.java

---
+++ src/test/java/net/apercova/quickcli/api/GetDateTest.java
@@ -0,0 +1,34 @@
+package net.percova.console.cli.api;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.apercova.quickcli.api.CommandFactory;
+import net.apercova.quickcli.api.TaskCommand;
+
+public class GetDateTest {
+	String[] args;
+	
+	@Before
+	public void init() {
+		args = new String[] {};
+	}
+	
+	@Test
+	public void test() throws Exception {
+		TaskCommand<Date> command = CommandFactory.createCommand(args, GetDate.class);	
+		assertTrue(command != null);
+		System.out.println(command.toString());
+				
+		
+		Date result = command.execute();
+		
+		assertTrue("Error printing current date", result != null);
+		System.out.println(result);
+		
+	}
+}
