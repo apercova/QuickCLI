2d65102f67e53c096daa2163a969acc1a85b99d2
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:09:41 -0600

    Create GetDate.java

---
+++ src/test/java/net/apercova/quickcli/api/GetDate.java
@@ -0,0 +1,19 @@
+package net.percova.console.cli.api;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import net.apercova.quickcli.api.BaseTaskCommand;
+import net.apercova.quickcli.api.CLICommand;
+
+@CLICommand(value="currdate", description="Retrieves current date.")
+public class GetDate extends BaseTaskCommand<Date>{
+
+	public GetDate() {
+	}
+	
+	@Override
+	public Date execute() throws Exception {
+		return Calendar.getInstance().getTime();
+	}
+}
