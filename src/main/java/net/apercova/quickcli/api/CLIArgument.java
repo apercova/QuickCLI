7b2c373f653c755e17335684b4497875eff82e2b
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:02:56 -0600

    Create CLIArgument.java

---
+++ src/main/java/net/apercova/quickcli/api/CLIArgument.java
@@ -0,0 +1,24 @@
+package net.apercova.quickcli.api;
+
+/**
+ * Command-line argument annotation
+ * 
+ * @author <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a> <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
+ * @since 1.0
+ *
+ */
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface CLIArgument {
+
+	String name();
+	String[] aliases() default {};
+	String value() default "";
+	boolean required() default false;
+	String usage() default "";
+}
