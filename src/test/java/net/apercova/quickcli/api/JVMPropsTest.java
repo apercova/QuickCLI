f2562e86272ad485017ac4a0d0308f883befe24a
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:10:33 -0600

    Create JVMPropsTest.java

---
+++ src/test/java/net/apercova/quickcli/api/JVMPropsTest.java
@@ -0,0 +1,40 @@
+package net.percova.console.cli.api;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import net.apercova.quickcli.api.ActionCommand;
+import net.apercova.quickcli.api.CommandFactory;
+
+public class JVMPropsTest {
+	String[] args;
+	
+	@Before
+	public void init() {
+		args = new String[] {
+				"--prop-name","java.runtime.version"
+		};
+	}
+	
+	@Test
+	public void test() throws Exception {
+		ActionCommand command = CommandFactory.createCommand(args, JVMProps.class);	
+		assertTrue(command != null);
+		System.out.println(command.toString());
+		
+		StringWriter out = new StringWriter();
+		PrintWriter writer = new PrintWriter(out, true);
+		command.setPrintWriter(writer);
+				
+		command.execute();
+		String result = out.toString();
+		assertTrue("Error printing JVM Properties", (result.startsWith("Begin JVM Properties")));
+		System.out.println(result);
+		
+	}
+}
