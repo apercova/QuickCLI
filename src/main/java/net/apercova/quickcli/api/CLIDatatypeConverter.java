be05200bd13edc3414df6d70c01f1de1b580d5e8
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:03:51 -0600

    Create CLIDatatypeConverter.java

---
+++ src/main/java/net/apercova/quickcli/api/CLIDatatypeConverter.java
@@ -0,0 +1,19 @@
+package net.apercova.quickcli.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Datatype converter annotation
+ * 
+ * @author <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a> <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
+ * @since 1.0
+ *
+ */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CLIDatatypeConverter {
+	Class<? extends DatatypeConverter<?>> value();
+}
