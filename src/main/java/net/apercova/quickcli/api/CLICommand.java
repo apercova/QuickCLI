89769ed6edc92cec6f0dbc3302c3e923eff72b85
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:03:35 -0600

    Create CLICommand.java

---
+++ src/main/java/net/apercova/quickcli/api/CLICommand.java
@@ -0,0 +1,20 @@
+package net.apercova.quickcli.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Command-line command annotation
+ * 
+ * @author <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a> <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
+ * @since 1.0
+ *
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CLICommand {
+	String value();
+	String description() default "";
+}
