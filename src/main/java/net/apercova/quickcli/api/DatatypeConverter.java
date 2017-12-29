c8ec60acab39f5278e1c49c6f41f4d4a83a80301
A. PerCova <alonsopc91@gmail.com>
2017-12-29 17:04:45 -0600

    Create DatatypeConverter.java

---
+++ src/main/java/net/apercova/quickcli/api/DatatypeConverter.java
@@ -0,0 +1,28 @@
+package net.apercova.quickcli.api;
+
+/**
+ * Datatype converter
+ * @param <T> Conversion destination type
+ * 
+ * @author <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a> <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
+ * @since 1.0
+ *
+ */
+public interface DatatypeConverter<T> {
+	
+	/**
+	 * Parses a value from a text source
+	 * @param value Source value
+	 * @return Parsed value
+	 * @throws DatatypeConverterException If an exception occurs
+	 */
+	public T parse(String value) throws DatatypeConverterException;
+	
+	/**
+	 * Retrieves a text-based object's representation
+	 * @param value Object value
+	 * @return Text-based object's representation
+	 * @throws DatatypeConverterException If an exception occurs
+	 */
+	public String marshall(T value) throws DatatypeConverterException;
+}
