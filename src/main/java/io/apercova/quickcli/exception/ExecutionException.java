5918a675fb9c417a1347fc1d869c05fb02189833
apercova <alonsopc91@gmail.com>
2019-04-11 13:03:19 -0500

    Formating text

--- src/main/java/io/apercova/quickcli/exception/ExecutionException.java
+++ src/main/java/io/apercova/quickcli/exception/ExecutionException.java
@@ -2,30 +2,31 @@ package io.apercova.quickcli.exception;
 
 /**
  * Command execution exception.
- * 
- * @author <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a> <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
+ *
+ * @author
+ * <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a>
+ * <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
  * @since 1.0
  *
  */
-public class ExecutionException extends Exception{
+public class ExecutionException extends Exception {
 
-	private static final long serialVersionUID = 4772821535245558588L;
+    private static final long serialVersionUID = 4772821535245558588L;
 
-	public ExecutionException() {
-		super();
-	}
+    public ExecutionException() {
+        super();
+    }
 
-	public ExecutionException(String message, Throwable cause) {
-		super(message, cause);
-	}
+    public ExecutionException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
-	public ExecutionException(String message) {
-		super(message);
-	}
+    public ExecutionException(String message) {
+        super(message);
+    }
 
-	public ExecutionException(Throwable cause) {
-		super(cause);
-	}
+    public ExecutionException(Throwable cause) {
+        super(cause);
+    }
 
-	
 }
